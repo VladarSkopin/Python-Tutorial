814e659e1d5d4ce08e9ca04690ceb24cfd5fed06
VladarSkopin <grudagor@gmail.com>
2022-11-28 22:42:45 +0300

    variables and basic IO

---
+++ first_projects/demo.py
@@ -0,0 +1,16 @@
+print(1, end=' ')
+print("1")
+print(2)
+print("2")
+
+age = 12
+name = 'Alex'
+print(name, "is", age, "years old.\nWelcome!")
+
+
+
+
+
+
+
+
---
+++ first_projects/demo_2.py
@@ -0,0 +1,18 @@
+
+print('Greetings! What\'s your name?')
+name = input()
+
+print('What a beautiful name. How old are you?')
+age = int (input())
+
+if age >= 18:
+    print('It\'s time to get busy...')
+else:
+    print('You are too young for now. Go play with your friends.')
+
+print('New user:', name, '\nAge:', age, 'years old')
+
+
+
+
+
