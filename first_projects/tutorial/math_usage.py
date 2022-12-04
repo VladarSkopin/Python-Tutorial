0f9e9801604c626fe31a26c8e3e24cc1c74ad2dd
VladarSkopin <grudagor@gmail.com>
2022-12-04 14:09:26 +0300

    math usage

---
+++ first_projects/tutorial/math_usage.py
@@ -0,0 +1,24 @@
+import math
+
+a = math.sqrt(49)
+b = math.pi
+c = math.pow(5, 3)
+d = math.ceil(0.55)
+e = math.e
+f = math.cos(math.radians(90))
+g = math.sin(math.radians(90))
+h = math.exp(10)
+i = math.inf
+j = math.log(64, 4)
+
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
