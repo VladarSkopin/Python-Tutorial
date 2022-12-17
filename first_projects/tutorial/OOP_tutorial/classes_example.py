430f19768fe752c8572c1eaa6d8d5921c29ead15
VladarSkopin <grudagor@gmail.com>
2022-12-17 12:49:44 +0300

    class constructor

--- first_projects/tutorial/OOP_tutorial/classes_example.py
+++ first_projects/tutorial/OOP_tutorial/classes_example.py
@@ -1,8 +1,11 @@
 class Person:
-    def __init__(self):  # constructor with initial fields
-        self.name = "n/a"
-        self.age = 0
-        self.salary = 0.0
+
+    num_of_eyes = 2
+
+    def __init__(self, name='n/a', age=0, salary=0.0):  # constructor with initial fields
+        self.name = name
+        self.age = age
+        self.salary = salary
 
     def print_person(self):
         print(f'    Name: {self.name}')
@@ -16,15 +19,8 @@ class Person:
 
 
 if __name__ == "__main__":
-    person_A = Person()
-    person_A.celebrate_birthday()
-    person_A.print_person()
+    person_1 = Person('Alex', 29, 2000)
+    person_1.print_person()
 
-    person_B = Person()
-    person_B.name = 'Alex'
-    person_B.age = 29
-    person_B.salary = 2000
-    person_B.celebrate_birthday()
-    person_B.print_person()
-
-
+    person_2 = Person()
+    person_2.print_person()
