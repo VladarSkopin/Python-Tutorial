842f61945398161f4debdbb7d6c7ea75c0388daa
Alexander Skopintsev <grudagor@gmail.com>
2023-03-04 23:12:20 -0800

    post requests

---
+++ first_projects/advanced_python/rest_api_tests/post_with_data.py
@@ -0,0 +1,9 @@
+import requests
+
+url = 'http://httpbin.org/post'
+payload = {
+    'website': 'dataquest.io',
+    'courses': ['Python', 'SQL']
+    }
+r = requests.post(url, data=payload)
+print(r.text)
